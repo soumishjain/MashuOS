7bf19188cca8d4e400638dfb55c4228460da6f40
soumishjain <jainmashu13@gmail.com>
2026-01-26 01:32:42 +0530

    draggable item created using rnd

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Dock from './Dock'
 import Nav from './Nav'
+import MainWindow from './Windows/MainWindow'
 
 const Main = () => {
   return (
     <div className='main'>
       <Nav />
       <Dock />
+
+      <MainWindow />
     </div>
   )
 }
