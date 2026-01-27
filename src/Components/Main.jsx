c78a71b1f249f4e2df4ebe0036c943eeb59726ce
soumishjain <jainmashu13@gmail.com>
2026-01-28 00:35:26 +0530

    notes section done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Dock from './Dock'
 import Nav from './Nav'
 import Github from './Windows/Github'
+import Note from './Windows/Note'
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <Nav />
       <Dock />
       <Github />
+      <Note />
     </div>
   )
 }
